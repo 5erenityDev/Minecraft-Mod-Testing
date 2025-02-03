4ffbe5fc86f66b61534b2e57ae670cda511b3232
Sean Blankenship <seanblankenship2002@gmail.com>
2025-02-03 00:08:28 -0500

    Adding Data directory
    Added data directory for block break and loot table conditions

---
+++ src/main/java/com/test/testmod/serenity/item/custom/MaceItem.java
@@ -0,0 +1,27 @@
+package com.test.testmod.serenity.item.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class MaceItem extends Item {
+    public MaceItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.testmod.mace_item.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+}
--- src/main/java/com/test/testmod/serenity/item/custom/TeleportStaffItem.java
+++ src/main/java/com/test/testmod/serenity/item/custom/TeleportStaffItem.java
@@ -5,7 +5,13 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class TeleportStaffItem extends Item {
     public TeleportStaffItem(Properties pProperties) {
@@ -37,5 +43,11 @@ public class TeleportStaffItem extends Item {
 
         pContext.getItemInHand().hurtAndBreak(1, pContext.getPlayer(), player -> player.broadcastBreakEvent(player.getUsedItemHand()));
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.testmod.telport_staff.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 }
